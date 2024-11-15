06c01433ad9845b837cebbcc26a4f1af3d7162e9
unknown <j3415879@naver.com>
2024-11-15 17:24:08 +0900

    d

--- src/main/front/src/App.js
+++ src/main/front/src/App.js
@@ -4,7 +4,7 @@ const App = () => {
   return (
     <div>
       <h1>Spring Boot React</h1>
-      <img src = "logo192.png"></img>
+      <img src = "logo192.png" alt = "dd"></img>
     </div>
   );
 };
