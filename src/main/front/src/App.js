e3cf33904e3baf60e6e2fac140029318f9184b13
unknown <j3415879@naver.com>
2024-11-15 14:40:03 +0900

    d

---
+++ src/main/front/src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const App = () => {
+  return (
+    <div>
+      <h1>Spring Boot React</h1>
+      <img src = "logo192.png"></img>
+    </div>
+  );
+};
+
+export default App;
